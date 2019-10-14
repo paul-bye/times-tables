cd56d314960b2c1768ab35608cbe5750a7f8bd11
Paul Bye <paul@pbye.co.uk>
2019-10-14 21:01:03 +0100

    Updates

---
+++ precache-manifest.7a5171766c062c7c6a4dcf17423cf7ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93faa33d4b381312e2ad",
+    "url": "/times-tables/static/js/runtime~main.93faa33d.js"
+  },
+  {
+    "revision": "d2482b9f5a3e00055d11",
+    "url": "/times-tables/static/js/main.d2482b9f.chunk.js"
+  },
+  {
+    "revision": "d4e2edbd0fdedb0a1f7a",
+    "url": "/times-tables/static/js/1.d4e2edbd.chunk.js"
+  },
+  {
+    "revision": "d2482b9f5a3e00055d11",
+    "url": "/times-tables/static/css/main.d2150cdf.chunk.css"
+  },
+  {
+    "revision": "cbc667f0c57500f8c3dbfbbfacabaade",
+    "url": "/times-tables/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/times-tables/precache-manifest.3e9bfaaac5f37563fc70502360c3022f.js"
+  "/times-tables/precache-manifest.7a5171766c062c7c6a4dcf17423cf7ce.js"
 );
 
 workbox.clientsClaim();
