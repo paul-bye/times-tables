cdf813723e3933563ab5cad9357f352066eb1634
Paul Bye <paul@pbye.co.uk>
2020-06-29 09:25:10 +0100

    Updates

--- precache-manifest.c7830638b0e964a78ac889cff781c10b.js
+++ precache-manifest.c7830638b0e964a78ac889cff781c10b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/times-tables/static/js/runtime~main.93faa33d.js"
   },
   {
-    "revision": "d2482b9f5a3e00055d11",
-    "url": "/times-tables/static/js/main.d2482b9f.chunk.js"
+    "revision": "ca0a1cba7a8b0228cf8d",
+    "url": "/times-tables/static/js/main.ca0a1cba.chunk.js"
   },
   {
     "revision": "d4e2edbd0fdedb0a1f7a",
     "url": "/times-tables/static/js/1.d4e2edbd.chunk.js"
   },
   {
-    "revision": "d2482b9f5a3e00055d11",
+    "revision": "ca0a1cba7a8b0228cf8d",
     "url": "/times-tables/static/css/main.d2150cdf.chunk.css"
   },
   {
-    "revision": "cbc667f0c57500f8c3dbfbbfacabaade",
+    "revision": "a311ba88fdc3ccda77fea15045803e3f",
     "url": "/times-tables/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/times-tables/precache-manifest.7a5171766c062c7c6a4dcf17423cf7ce.js"
+  "/times-tables/precache-manifest.c7830638b0e964a78ac889cff781c10b.js"
 );
 
 workbox.clientsClaim();
